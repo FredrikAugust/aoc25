54a0f924afa73973d229aa30ca9591340c3743fa
Fredrik A. Madsen-Malmo <fredrik.malmo@icloud.com>
2025-12-01 13:54:20 +0100

    solve day1

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/fredrikaugust/aoc25/solutions"
+import (
+	"log/slog"
+
+	"github.com/fredrikaugust/aoc25/solutions"
+)
 
 func main() {
+	slog.SetLogLoggerLevel(slog.LevelDebug)
 	solutions.Day1A()
+	solutions.Day1B()
 }
--- solutions/01.go
+++ solutions/01.go
@@ -43,5 +43,50 @@ func Day1A() {
 		slog.Debug("update", "dir", string(line[0]), "amount", line[1:], "new pos", pos)
 	}
 
-	slog.Info("done", "result", zeroCount)
+	slog.Info("done part1", "result", zeroCount)
+}
+
+func Day1B() {
+	input := utils.GetInput(1)
+
+	pos := 50
+	zeroCount := 0
+
+	for line := range strings.SplitSeq(input, "\n") {
+		if line == "" {
+			continue
+		}
+
+		switch line[0] {
+		case 'L':
+			value, _ := strconv.Atoi(line[1:])
+			rotateLeft(&pos, &zeroCount, value)
+		case 'R':
+			value, _ := strconv.Atoi(line[1:])
+			rotateRight(&pos, &zeroCount, value)
+		}
+	}
+
+	slog.Info("done part2", "result", zeroCount)
+}
+
+func rotateLeft(pos, zeroCount *int, value int) {
+	for _ = range value {
+		(*pos)--
+		if *pos == 0 {
+			(*zeroCount)++
+		} else if *pos < 0 {
+			*pos = 99
+		}
+	}
+}
+
+func rotateRight(pos, zeroCount *int, value int) {
+	for _ = range value {
+		(*pos)++
+		if *pos == 100 {
+			(*zeroCount)++
+			*pos = 0
+		}
+	}
 }
